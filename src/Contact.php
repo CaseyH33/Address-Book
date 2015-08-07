eed995d5880e2ab1d38526b867437275e7e133da
Casey Heitz <heitz.casey@gmail.com>
2015-08-07 09:39:18 -0700

    Adds in Contact class with setters, getters, save, getAll, and deleteAll functions

---
+++ src/Contact.php
@@ -0,0 +1,60 @@
+<?php
+  class Contact
+  {
+    private $name;
+    private $phone_number;
+    private $address;
+
+    function __construct($name, $number, $address)
+    {
+      $this->name = $name;
+      $this->phone_number = $number;
+      $this->address = $address;
+    }
+
+    function getName()
+    {
+      return $this->name;
+    }
+
+    function setName($new_name)
+    {
+      $this->name = $new_name;
+    }
+
+    function getPhoneNumber()
+    {
+      return $this->$phone_number;
+    }
+
+    function setPhoneNumber($new_number)
+    {
+      $this->phone_number = $new_number;
+    }
+
+    function getAddress()
+    {
+      return $this->address;
+    }
+
+    function setAddress($new_address)
+    {
+      $this->address = $new_address;
+    }
+
+    function save()
+    {
+      array_push($_SESSION['list_of_contacts'], $this);
+    }
+
+    static function getAll()
+    {
+      return $_SESSION['list_of_contacts'];
+    }
+
+    static function deleteAll()
+    {
+      $_SESSION['list_of_contacts'] = array();
+    }
+  }
+?>
