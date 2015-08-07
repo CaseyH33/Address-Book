1e1a827fe436a55e4eff4db6576c0208c7e654b1
Casey Heitz <heitz.casey@gmail.com>
2015-08-07 09:48:25 -0700

    Adds app.php functionality

---
+++ app/app.php
@@ -0,0 +1,36 @@
+<?php
+
+  require_once __DIR__."/../vendor/autoload.php";
+  require_once __DIR__."/../src/Contact.php";
+
+  session_start();
+
+  if(empty($_SESSION['list_of_contacts']))
+  {
+    $_SESSION['list_of_contacts'] = array();
+  }
+
+  $app = new Silex\Application();
+
+  $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+  ));
+
+  $app->get("/", function() use ($app) {
+    return $app['twig']->render('contacts.html.twig', array('contacts' => Contact::getAll()));
+  });
+
+  $app->post("/contacts", function() use ($app) {
+    $contact = new Contact($_POST['name', 'phone_number', 'address']);
+    $contact->save();
+    return $app['twig']->render('create_contact.html.twig', array('new_contact' => $contact));
+  });
+
+  $app->post("/delete_contacts", function () use ($app) {
+    Contact::deleteAll();
+    return $app['twig']->render('delete_contacts.html.twig');
+  });
+
+  return $app;
+
+?>
